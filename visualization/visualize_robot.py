418b0704c121de603130841839f6935900ef39e1
Grégoire Passault <g.passault@gmail.com>
2023-11-20 21:44:28 +0100

    Robot visualization example

---
+++ visualization/visualize_robot.py
@@ -0,0 +1,32 @@
+from placo_utils.visualization import robot_viz, robot_frame_viz
+from ischedule import schedule, run_loop
+import numpy as np
+import placo
+
+"""
+Visualizes a 6-axis robot
+"""
+
+robot = placo.RobotWrapper("../models/6axis/", placo.Flags.ignore_collisions)
+viz = robot_viz(robot)
+
+t = 0
+dt = 0.01
+
+@schedule(interval=dt)
+def loop():
+    global t
+    t += dt
+
+    # Moving some DofS
+    robot.set_joint('r1', np.sin(t))
+    robot.set_joint('r2', np.sin(t/3))
+
+    # Showing effector frame
+    robot.update_kinematics()
+    robot_frame_viz(robot, "effector")
+
+    # Updating the viewer
+    viz.display(robot.state.q)
+
+run_loop()
