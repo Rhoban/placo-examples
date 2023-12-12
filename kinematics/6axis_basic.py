73d6c1fc5b86ddeeac3006c0b1cd496fb8c6fe83
Grégoire Passault <g.passault@gmail.com>
2023-12-12 22:12:32 +0100

    Intro example

---
+++ kinematics/6axis_basic.py
@@ -0,0 +1,47 @@
+import placo
+import numpy as np
+import pinocchio as pin
+from ischedule import schedule, run_loop
+from placo_utils.visualization import robot_viz, robot_frame_viz, frame_viz
+from placo_utils.tf import tf
+
+"""
+6axis robot reaching a given target (introduction example).
+"""
+
+# Loading the robot
+robot = placo.RobotWrapper("../models/6axis", placo.Flags.ignore_collisions)
+
+# Creating the solver
+solver = placo.KinematicsSolver(robot)
+solver.mask_fbase(True)
+
+effector_task = solver.add_frame_task("effector", np.eye(4))
+effector_task.configure("effector", "soft", 1.0, 1.0)
+
+viz = robot_viz(robot)
+
+t = 0
+dt = 0.01
+solver.dt = dt
+
+
+@schedule(interval=dt)
+def loop():
+    global t, last_target
+    t += dt
+
+    # Updating the target
+    effector_task.T_world_frame = tf.translation_matrix([1.25, np.sin(t), 1.0])
+
+    # Solving the IK
+    robot.update_kinematics()
+    solver.solve(True)
+
+    # Displaying the robot, effector and target
+    viz.display(robot.state.q)
+    robot_frame_viz(robot, "effector")
+    frame_viz("target", effector_task.T_world_frame)
+
+
+run_loop()
