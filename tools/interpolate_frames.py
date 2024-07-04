cbc31a411a830f4c56ce9eff8f9343c2b6376116
Grégoire Passault <g.passault@gmail.com>
2024-07-04 20:02:26 +0200

    Conistent naming with the method

--- tools/interpolate_frames.py
+++ tools/interpolate_frames.py
@@ -12,10 +12,10 @@ This will result in an animation with the interpolated frame moving between two 
 """
 
 
-T_world_frame1 = tf.translation_matrix([0.1, -0.2, 0.3]) @ tf.rotation_matrix(
+T_world_frameA = tf.translation_matrix([0.1, -0.2, 0.3]) @ tf.rotation_matrix(
     np.pi / 4, [0, 0, 1]
 )
-T_world_frame2 = tf.translation_matrix([0.2, 0.4, 0.4]) @ tf.rotation_matrix(
+T_world_frameB = tf.translation_matrix([0.2, 0.4, 0.4]) @ tf.rotation_matrix(
     1.3 * np.pi, [0, 1.0, 0]
 )
 t = 0
@@ -34,11 +34,11 @@ def loop():
         alpha = 1 - (t - 2) / 2
 
     T_world_interpolate = placo.interpolate_frames(
-        T_world_frame1, T_world_frame2, alpha
+        T_world_frameA, T_world_frameB, alpha
     )
 
-    frame_viz("frame1", T_world_frame1)
-    frame_viz("frame2", T_world_frame2)
+    frame_viz("frameA", T_world_frameA)
+    frame_viz("frameB", T_world_frameB)
     frame_viz("interpolated", T_world_interpolate, opacity=0.5)
 
 
